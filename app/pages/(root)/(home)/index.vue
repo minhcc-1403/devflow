62ca3d18e2b15dfb37b71152d261f9fa915b7c51
minh <minhchiu.official@gmail.com>
2024-11-21 00:13:00 +0700

    feat: home route

--- app/pages/(root)/(home)/index.vue
+++ app/pages/(root)/(home)/index.vue
@@ -5,8 +5,17 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
-    <DataGrid />
-    <DataCharts />
+  <div class="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
+    <h1 class="h1-bold text-dark100_light900">All Questions</h1>
+
+    <NuxtLink to="/ask-question" class="flex justify-end max-sm:w-full">
+      <Button class="primary-gradient min-h-[46px] px-4 py-3 text-gray-100"
+        >Ask a Question</Button
+      >
+    </NuxtLink>
+  </div>
+
+  <div class="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+    LocalSearchbar Filters
   </div>
 </template>
