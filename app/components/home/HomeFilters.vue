7fb2b502b7e668c97dade7209b301b56de4e3482
minh <minhchiu.official@gmail.com>
2024-12-15 14:37:49 +0700

    manage tag filter state

--- app/components/home/HomeFilters.vue
+++ app/components/home/HomeFilters.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { HomePageFilters } from "~/utils/constants/filters";
 
-const active = "newest";
+const urlSearchParam = useUrlSearchParams();
+const active = ref(urlSearchParam.tag?.toString() || "");
+
+const handleTypeClick = (item: string) => {
+  if (active.value === item) {
+    delete urlSearchParam.tag;
+    active.value = "";
+  } else {
+    urlSearchParam.tag = item;
+    active.value = item;
+  }
+};
 </script>
 
 <template>
@@ -10,7 +21,8 @@ const active = "newest";
       v-for="item in HomePageFilters"
       :key="item.value"
       class="body-medium rounded-lg px-6 py-3 capitalize shadow-none"
-      :class="`${active === item.value ? 'bg-main-100 text-main-500' : 'bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:hover:bg-dark-400 dark:text-gray-100'}`"
+      :class="`${active === item.value ? 'bg-main-100 text-main-500 hover:bg-main-100/80' : 'bg-light-800 text-light-500 hover:bg-light-800/90 dark:bg-dark-300 dark:text-gray-100 dark:hover:bg-dark-300/80'}`"
+      @click="handleTypeClick(item.value)"
     >
       {{ item.name }}
     </Button>
